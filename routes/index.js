0dfce859fbd6827240133860420914ac1c78194a
Henrique Belém <115942716+HrickBel@users.noreply.github.com>
2023-11-11 20:28:55 +0000

    waaaaaaaaaaa

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,32 @@ const bodyParser = require('body-parser');
 var express = require('express');
 var router = express.Router();
 
+let res;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	Message.findAll({attributes:['name','message']}).then(result => {
+	Message.findAll({attributes:['name','message'],raw:true}).then(result => {
+		console.log(result);
+		res = result;
 	});
-	res.render('index',{message:'aa'});
+	res.render('index',{res:res});
 });
 
 
 router.get('/message', function(req, res, next){
-	res.render('message',{title:'message'});
+	res.render('message');
 });
 
 router.post('/register', async function(req,res){
-	Message.create({name: req.body.name , message: req.body.message}).then(
-		() => {
-			console.log('mensagem registrada com sucesso ', req.body.name ,' ', req.body.message);
-			res.render('index');
-		}).catch(
-		() => {
-			console.log('mensagem não registrada');
-			res.render('message');
+	if(req.body.name !== '' && req.body.message !== ''){
+		Message.create({name: req.body.name , message: req.body.message}).then(function(){
+			console.log('mensagem registrada \nnome:',req.body.name,'\nmensagem:', req.body.message);
+			res.redirect('/');
 		});
+	}else{
+		console.log('mensagem não registrada');
+		res.redirect('message');
+	}
 });
 
 module.exports = router;
