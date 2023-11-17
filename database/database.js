e0e92adda03bed79bc2c932e246b02d7904b818b
Henrique Belém <115942716+HrickBel@users.noreply.github.com>
2023-11-17 07:35:38 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-	'heroku_c9802116e3bd2d2',
-	'bb73019fd002dc',
-	'ce1a5d83',
+	'',
+	'',
+	'',
 	{
-		host:'us-cdbr-east-06.cleardb.net',
+		host:'',
 		dialect:'mysql'
 	}
 );
